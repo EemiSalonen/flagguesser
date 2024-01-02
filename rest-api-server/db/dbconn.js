7c543cddbcc0942376b75658a6a89bcd99e69cf8
EemiSalonen <eemi.salonen@gmail.com>
2024-01-02 15:43:49 +0200

    changed db configuration

--- rest-api-server/db/dbconn.js
+++ rest-api-server/db/dbconn.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 class MongoConnection {
-  constructor() {
-    this._connect();
-  }
+	constructor() {
+		this._connect();
+	}
 
-  _connect() {
-    mongoose
-      .connect(`mongodb://root:password@localhost:27017/?authMechanism=DEFAULT`)
-      .then(() => console.log('Database connection successfull!'))
-      .catch(() => console.log('Database connection failed!'));
-  }
+	_connect() {
+		mongoose
+			.connect(process.env.DB_STRING)
+			.then(() => console.log("Database connection successfull!"))
+			.catch(() => console.log("Database connection failed!"));
+	}
 }
 
 module.exports = new MongoConnection();
